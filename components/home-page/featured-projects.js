bb6c45d818b019f32f32f353ab4e019f1c695ae2
Churchil Owino <chachowino75@gmail.com>
2023-04-13 18:27:18 +0300

    Create featured projects cmp

---
+++ components/home-page/featured-projects.js
@@ -0,0 +1,11 @@
+import classes from "./featured-projects.module.css";
+
+function FeaturedProjects() {
+  return (
+    <section className={classes.projects}>
+      <h2>Featured Projects</h2>
+    </section>
+  );
+}
+
+export default FeaturedProjects;
