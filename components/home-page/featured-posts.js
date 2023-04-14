529af37ce0b9389eb71a84a0640ad98598b2a35e
Churchil Owino <chachowino75@gmail.com>
2023-04-14 17:30:55 +0300

    Add layout icons to cmp

--- components/home-page/featured-posts.js
+++ components/home-page/featured-posts.js
@@ -1,11 +1,20 @@
 import PostsGrid from "../posts/posts-grid";
 import classes from "./featured-posts.module.css";
+import { TbWriting } from "react-icons/tb";
 
 function FeaturedPosts(props) {
   return (
-    <section className={classes.posts}>
-      <h2>Featured Blogs</h2>
-      <PostsGrid posts={props.posts} />
+    <section>
+      <div className="topsvg">
+        <TbWriting />
+      </div>
+      <div className={classes["posts-list"]}>
+        <h2>Latest blogs</h2>
+        <PostsGrid posts={props.posts} />
+      </div>
+      <div className="botsvg">
+        <TbWriting />
+      </div>
     </section>
   );
 }
