2b8fb1f5dd4fc0f70a9111eff796294f899a037a
Churchil Owino <chachowino75@gmail.com>
2023-04-14 17:33:28 +0300

    Add layout icons to cmp

--- components/posts/all-posts.js
+++ components/posts/all-posts.js
@@ -1,11 +1,20 @@
 import classes from "./all-posts.module.css";
 import PostsGrid from "./posts-grid";
+import { TbWriting } from "react-icons/tb";
 
 function AllPosts(props) {
   return (
-    <section className={classes.posts}>
-      <h1>All Blogs</h1>
-      <PostsGrid posts={props.posts} />
+    <section>
+      <div className="topsvg">
+        <TbWriting />
+      </div>
+      <div className={classes["posts-list"]}>
+        <h2 className="special-h2">All Blogs</h2>
+        <PostsGrid posts={props.posts} />
+      </div>
+      <div className="botsvg">
+        <TbWriting />
+      </div>
     </section>
   );
 }
