d597d21eab3547b285f2d6eec5b30a172f468e74
Churchil Owino <chachowino75@gmail.com>
2023-04-17 22:52:06 +0300

    Create all projects cmp

---
+++ components/projects/all-projects.js
@@ -0,0 +1,23 @@
+import { BsCodeSlash } from "react-icons/bs";
+
+import classes from "./all-projects.module.css";
+import ProjectsList from "./projects-list";
+
+function AllProjects(props) {
+  return (
+    <section>
+      <div className="topsvg">
+        <BsCodeSlash />
+      </div>
+      <div className={classes["projects-list"]}>
+        <h2 className="special-h2">All projects</h2>
+        <ProjectsList projects={props.projects} />
+      </div>
+      <div className="botsvg">
+        <BsCodeSlash />
+      </div>
+    </section>
+  );
+}
+
+export default AllProjects;
