c5590f1a1317d79b832c8f44040a3b763e165a41
Churchil Owino <chachowino75@gmail.com>
2023-04-18 22:11:15 +0300

    Set up navbar to dissappear and appear on scroll Add active class to nav links

--- components/layout/main-nav.js
+++ components/layout/main-nav.js
@@ -1,10 +1,46 @@
 import Link from "next/link";
 import classes from "./main-nav.module.css";
 import Logo from "./logo";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+function useScrollDirection() {
+  const [scrollDirection, setScrollDirection] = useState(null);
+
+  useEffect(() => {
+    let lastScrollY = window.pageYOffset;
+
+    const updateScrollDirection = () => {
+      const scrollY = window.pageYOffset;
+      const direction = scrollY > lastScrollY ? "down" : "up";
+      if (
+        direction !== scrollDirection &&
+        (scrollY - lastScrollY > 3 || scrollY - lastScrollY < -3)
+      ) {
+        setScrollDirection(direction);
+      }
+      lastScrollY = scrollY > 0 ? scrollY : 0;
+    };
+    window.addEventListener("scroll", updateScrollDirection); // add event listener
+    return () => {
+      window.removeEventListener("scroll", updateScrollDirection); // clean up
+    };
+  }, [scrollDirection]);
+
+  return scrollDirection;
+}
 
 function MainNav() {
+  const router = useRouter();
+
+  const scrollDirection = useScrollDirection();
+
   return (
-    <header className={classes.header}>
+    <header
+      className={`${classes.header} ${
+        scrollDirection === "down" ? classes.down : ""
+      }`}
+    >
       <div>
         <Link href="/">
           <Logo />
@@ -13,13 +49,28 @@ function MainNav() {
       <nav>
         <ul>
           <li>
-            <Link href="/projects">Projects</Link>
+            <Link
+              href="/projects"
+              className={router.pathname === "/projects" ? classes.active : ""}
+            >
+              Projects
+            </Link>
           </li>
           <li>
-            <Link href="/posts">Blogs</Link>
+            <Link
+              href="/posts"
+              className={router.pathname === "/posts" ? classes.active : ""}
+            >
+              Blogs
+            </Link>
           </li>
           <li>
-            <Link href="/contact">Contact</Link>
+            <Link
+              href="/contact"
+              className={router.pathname === "/contact" ? classes.active : ""}
+            >
+              Contact
+            </Link>
           </li>
         </ul>
       </nav>
