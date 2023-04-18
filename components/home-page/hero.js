6a93eed26f7e3aba71453601b2f0b0ec8c0b0354
Churchil Owino <chachowino75@gmail.com>
2023-04-18 22:10:14 +0300

    Use animated letters cmp

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -1,7 +1,119 @@
 import Image from "next/image";
 import classes from "./hero.module.css";
+import AnimatedLetters from "../ui/animated-letters";
+import { useState } from "react";
 
 function Hero() {
+  const [letterClass, setLetterClass] = useState("text-animate");
+
+  const nameArray = [
+    "H",
+    "i",
+    ",",
+    " ",
+    "I",
+    "'",
+    "m",
+    " ",
+    "C",
+    "h",
+    "u",
+    "r",
+    "c",
+    "h",
+    "i",
+    "l",
+  ];
+
+  const jobArray = [
+    "I",
+    " ",
+    "l",
+    "i",
+    "v",
+    "e",
+    " ",
+    "a",
+    "n",
+    "d",
+    " ",
+    "b",
+    "r",
+    "e",
+    "a",
+    "t",
+    "h",
+    "e",
+    " ",
+    "s",
+    "o",
+    "f",
+    "t",
+    "w",
+    "a",
+    "r",
+    "e",
+    " ",
+    "d",
+    "e",
+    "v",
+    "e",
+    "l",
+    "o",
+    "p",
+    "m",
+    "e",
+    "n",
+    "t",
+    " ",
+    "|",
+    " ",
+    "J",
+    "a",
+    "v",
+    "a",
+    "S",
+    "c",
+    "r",
+    "i",
+    "p",
+    "t",
+    ",",
+    " ",
+    "R",
+    "e",
+    "a",
+    "c",
+    "t",
+    ".",
+    "j",
+    "s",
+    ",",
+    " ",
+    "N",
+    "e",
+    "x",
+    "j",
+    ".",
+    "j",
+    "s",
+    ",",
+    " ",
+    "C",
+    "S",
+    "S",
+    "3",
+    " ",
+    "&",
+    " ",
+    "H",
+    "T",
+    "M",
+    "L",
+    "5",
+    ".",
+  ];
+
   return (
     <section className={classes.hero}>
       <div className={classes.image}>
@@ -13,10 +125,19 @@ function Hero() {
         />
       </div>
       <div className={classes.content}>
-        <h1>Hi, I'm Churchil</h1>
+        <h1>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
+        </h1>
         <p>
-          I live and breathe front-end software development | JavaScript, React.js,
-          Next.js, & CSS3.
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={31}
+          />
         </p>
       </div>
     </section>
