d4e1f443878cc9d81d249397b5a257899ee4499c
Churchil Owino <chachowino75@gmail.com>
2023-04-19 21:26:30 +0300

    Add and use useInView

--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -1,8 +1,12 @@
 import Link from "next/link";
 import Image from "next/image";
 import classes from "./post-item.module.css";
+import { useRef } from "react";
+import { useInView } from "framer-motion";
 
 function PostItem(props) {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true, amount: 0.25 });
   const { image, date, title, excerpt, slug } = props.post;
 
   const formattedDate = new Date(date).toLocaleDateString("en-US", {
@@ -16,7 +20,10 @@ function PostItem(props) {
   const linkPath = `/posts/${slug}`;
 
   return (
-    <li className={classes.post}>
+    <li
+      className={`${classes.post} ${isInView ? classes.inview : ""}`}
+      ref={ref}
+    >
       <Link href={linkPath}>
         <div className={classes.image}>
           <Image src={imagePath} alt={title} width={300} height={200} />
